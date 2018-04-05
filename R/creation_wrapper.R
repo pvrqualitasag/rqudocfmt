182b9a8f8993d629ac1f98c8832e680c5b96cb6e
pvr <pvr@pvrs-MacBook-Pro.local>
2018-04-05 15:30:21 +0200

    First version after changing to new templates

--- R/creation_wrapper.R
+++ R/creation_wrapper.R
@@ -6,10 +6,10 @@
 ###
 ### #################################################### ###
 
-#' Create a document from template qualitas_project_ph_spez_de
+#' Create a document from template quag_ph_spez
 #'
 #' This function is a wrapper to create a document that produces
-#' pdf output and that uses the template "qualitas_project_ph_spez_de"
+#' pdf output and that uses the template "quag_ph_spez"
 #'
 #' Per default the new document is created in a separate subdirectory,
 #' is not overwritten if it already exists, is open to be edited
@@ -23,17 +23,21 @@
 #' @param   pbOverwrite          flag whether existing files are overwritten
 #' @param   pbEdit               directly open newly created document
 #' @param   plReplace            list with replacement key-values
-#' @export  create_qupps_de
-create_qupps_de <- function(psDocuName,
-                            psPkgPath         = ".",
-                            psDocuSubdir      = "vignettes",
-                            pDocuHasOwnSubdir = TRUE,
-                            pbOverwrite       = FALSE,
-                            pbEdit            = TRUE,
-                            plReplace         = NULL){
+#' @examples
+#' \dontrun{
+#' rqudocfmt::create_quag_ph_spez(psDocuName = "my_doc")
+#' }
+#' @export create_quag_ph_spez
+create_quag_ph_spez <- function(psDocuName,
+                                psPkgPath         = ".",
+                                psDocuSubdir      = "vignettes",
+                                pDocuHasOwnSubdir = TRUE,
+                                pbOverwrite       = FALSE,
+                                pbEdit            = TRUE,
+                                plReplace         = NULL){
   rmddochelper::create_pdf_output_skeleton(psDocuName        = psDocuName,
                                            psPkgPath         = psPkgPath,
-                                           psRmdTemplate     = "qualitas_project_ph_spez_de",
+                                           psRmdTemplate     = "quag_ph_spez",
                                            psTemplatePkg     = "rqudocfmt",
                                            psDocuSubdir      = psDocuSubdir,
                                            pDocuHasOwnSubdir = pDocuHasOwnSubdir,
@@ -43,10 +47,10 @@ create_qupps_de <- function(psDocuName,
 }
 
 
-#' Create a document from template qualitas_project_ph_spez_en
+#' Create a document from template quag_empty
 #'
 #' This function is a wrapper to create a document that produces
-#' pdf output and that uses the template "qualitas_project_ph_spez_en"
+#' pdf output and that uses the template "quag_empty"
 #'
 #' Per default the new document is created in a separate subdirectory,
 #' is not overwritten if it already exists, is open to be edited
@@ -60,8 +64,12 @@ create_qupps_de <- function(psDocuName,
 #' @param   pbOverwrite          flag whether existing files are overwritten
 #' @param   pbEdit               directly open newly created document
 #' @param   plReplace            list with replacement key-values
-#' @export  create_qupps_en
-create_qupps_en <- function(psDocuName,
+#' @examples
+#' \dontrun{
+#' rqudocfmt::create_quag_empty(psDocuName = "my_doc")
+#' }
+#' @export  create_quag_empty
+create_quag_empty <- function(psDocuName,
                             psPkgPath         = ".",
                             psDocuSubdir      = "vignettes",
                             pDocuHasOwnSubdir = TRUE,
@@ -70,7 +78,48 @@ create_qupps_en <- function(psDocuName,
                             plReplace         = NULL){
   rmddochelper::create_pdf_output_skeleton(psDocuName        = psDocuName,
                                            psPkgPath         = psPkgPath,
-                                           psRmdTemplate     = "qualitas_project_ph_spez_en",
+                                           psRmdTemplate     = "quag_empty",
+                                           psTemplatePkg     = "rqudocfmt",
+                                           psDocuSubdir      = psDocuSubdir,
+                                           pDocuHasOwnSubdir = pDocuHasOwnSubdir,
+                                           pbOverwrite       = pbOverwrite,
+                                           pbEdit            = pbEdit,
+                                           plReplace         = plReplace)
+}
+
+
+#' Create a document from template quag_empty_cover
+#'
+#' This function is a wrapper to create a document that produces
+#' pdf output and that uses the template "quag_empty_cover"
+#'
+#' Per default the new document is created in a separate subdirectory,
+#' is not overwritten if it already exists, is open to be edited
+#' directly and does not pass on any information for pattern
+#' replacement.
+#'
+#' @param   psDocuName           name of the new document
+#' @param   psPkgPath            path where package is located under which document should be created
+#' @param   psDocuSubdir         subdirectory in which document should be saved to
+#' @param   pbDocuHasOwnSubdir   should document be stored in separate subdir
+#' @param   pbOverwrite          flag whether existing files are overwritten
+#' @param   pbEdit               directly open newly created document
+#' @param   plReplace            list with replacement key-values
+#' @examples
+#' \dontrun{
+#' rqudocfmt::create_quag_empty_cover(psDocuName = "my_doc")
+#' }
+#' @export create_quag_empty_cover
+create_quag_empty_cover <- function(psDocuName,
+                              psPkgPath         = ".",
+                              psDocuSubdir      = "vignettes",
+                              pDocuHasOwnSubdir = TRUE,
+                              pbOverwrite       = FALSE,
+                              pbEdit            = TRUE,
+                              plReplace         = NULL){
+  rmddochelper::create_pdf_output_skeleton(psDocuName        = psDocuName,
+                                           psPkgPath         = psPkgPath,
+                                           psRmdTemplate     = "quag_empty_cover",
                                            psTemplatePkg     = "rqudocfmt",
                                            psDocuSubdir      = psDocuSubdir,
                                            pDocuHasOwnSubdir = pDocuHasOwnSubdir,
